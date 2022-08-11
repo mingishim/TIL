899f8d74e7c68b15104e6b2b6ee7a399c42a4f08
심민기 <shim102819@naver.com>
2022-08-11 21:48:37 +0900

    good

---
+++ team-project/project_escape/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <Todo></Todo>
+    </div>
+</template>
+<script>
+import Todo from '../components/page3/todo2'
+
+
+export default {
+    components: {
+    'Todo' : Todo
+    },
+};
+
+</script>
+
